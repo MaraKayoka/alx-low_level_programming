578f78bc5bb1407c2f2a984000ef170ff6da9f66
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 16:39:11 -0700

    desordre

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that prints using the function puts 
+ * main - a program that prints using the function puts
  * Return: 0
 */
-int main(void) 
-{
+int main(void)
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

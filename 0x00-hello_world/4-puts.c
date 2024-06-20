2183583afe26baba8ca67d9449844422e927f414
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 16:30:28 -0700

    ca_va_peter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - a program that prints using the function puts 
  * Return: 0
 */
-
-int main (void) {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-

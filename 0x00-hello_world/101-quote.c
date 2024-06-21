3571a6663cb271040048d76f6cb22ccb1d7751c5
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 19:58:08 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that writes output error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stout);
+return (1);
+}

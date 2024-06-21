af73a8d74bde887d119a0fcb00f563d0db16d9f9
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 17:36:18 -0700

    data_types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that writes various type of data
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(inttype));
+	printf("Size of float: %zu bytes\n", sizeof(floattype));
+	printf("Size of double: %zu bytes\n", sizeof(floattype));
+	printf("Size of char: %zu bytes\n", sizeof(chartype));
+	return (0);
+	}

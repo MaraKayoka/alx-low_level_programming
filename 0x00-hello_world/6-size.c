b583f77430fb92e9c692baa31bfdec2f6f245794
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 17:56:09 -0700

    data_type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long int longintType;
 	long long int longlongintType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(floatType));
-	printf("Size of char: %zu bytes\n", sizeof(charType));
-	printf("Size of long int: %zu bytes\n", sizeof(longintType));
-	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of int: %zu byte\n", sizeof(intType));
+	printf("Size of float: %zu byte\n", sizeof(floatType));
+	printf("Size of double: %zu byte\n", sizeof(floatType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of long int: %zu byte\n", sizeof(longintType));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
 	return (0);
 	}

fddaf2bef74526ffd8d2c79920850597b1a1fded
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 20:04:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unstd.h>
 /**
  * main - a program that writes output error
  * Return: 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stout);
 return (1);
 }

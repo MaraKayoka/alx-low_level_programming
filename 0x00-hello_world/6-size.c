431647348cd39a05c36d65c99f2c4aeb09c5daa4
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 19:39:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int d;
 long long int f;
 float e;
 
-printf("Size of chart: %lu byte(s)\n", (unsigned long) sizeof(a));
-printf("Size of int: %lu byte(s)\n", (unsigned long) sizeof(b));
-printf("Size of long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long) sizeof(f));
-printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof(e));
+printf("Size of chart: %lu byte(s),\n", (unsigned long) sizeof(a));
+printf("Size of int: %lu byte(s),\n", (unsigned long) sizeof(b));
+printf("Size of long int: %lu byte(s),\n", (unsigned long) sizeof(d));
+printf("Size of long long int: %lu byte(s),\n", (unsigned long) sizeof(f));
+printf("Size of float: %lu byte(s),\n", (unsigned long) sizeof(e));
 return (0);
 	}

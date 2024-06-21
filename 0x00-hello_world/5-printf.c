0f4ef9e03cddaea5d40786f9269468904b89212b
Mara Kayoka <marakayoka@gmail.com>
2024-06-20 17:02:09 -0700

    printing stuff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,", file=sys.stderr);
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }

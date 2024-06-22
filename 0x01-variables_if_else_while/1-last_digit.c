96fc28dabdf7ee7a7dbdb74459d7b3f3cf1927fa
Mara Kayoka <marakayoka@gmail.com>
2024-06-21 19:38:00 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	} else if (n < 6 && n != 0)
 	{
 		printf("%s %d %s %s\n", str1, n, str3, str4);
-	} else
+	} else if (n == 0)
 	{
 		printf("%s %d %s and is 0\n", str1, n, str3);
 	}

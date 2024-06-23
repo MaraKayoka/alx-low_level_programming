9d873fc070b64be255729b2386050ed8c4ee05ee
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 04:09:13 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints fferent combinations of two digit
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int n, k;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (k = 'n + 1'; k <= '9'; k++)
+		{
+			putchar(j);
+		} putchar(',')
+		putchar(' ')
+	}
+
+	return (0);
+}

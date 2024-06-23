b666daeeb1a1007a1b9382b1fd4d287f530d75f6
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 02:55:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabet in reverse
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = tolower(ch);
+
+	for (ch = 'Z'; ch <= 'A'; ch--)
+	{
+		putchar(tolower(ch));
+	} putchar('\n')
+	return (0);
+}

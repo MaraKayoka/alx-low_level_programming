cf9ce55d628f1e400d0bc66eb30e416e22d083f7
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 02:36:07 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that writes numers
+ * Return: Always 0(success)
+ */
+/* Betty always there for a checkin */
+
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0'; n <= '10'; n++)
+	{
+		putchar(n);
+	} putchar('\n')
+

58ae6d0fb6d58566abbe7cf9ba4e134d62c51456
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 01:43:11 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - a program that prints the alphabet in lc then upc
+ * Return: Always 0(Success)
+ */
+/* Always remember your wife betty*/
+
+int main(void)
+{
+	char ch = 'A';
+
+	ch = tolower(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+	{
+		putchar(tolower(ch));
+		putchar(ch);
+	} putchar('\n');
+	return (0);
+}

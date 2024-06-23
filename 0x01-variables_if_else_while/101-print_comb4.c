893db9126c93b2940724bb02d2ebb1964302a940
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 05:15:33 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - a program that print the combianaison of three numbers
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int n, j, k;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (j = n + 1; j <= '9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				if (n != j != k)
+				{
+					putchar(n);
+					putchar(j);
+					putchar(k);
+					if (n != '9' || j != '9' || k != '8')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}

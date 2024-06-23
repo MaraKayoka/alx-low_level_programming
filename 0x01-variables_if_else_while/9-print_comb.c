3a5ec205307b45fb37597d1f48ff17549738f340
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 03:34:39 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stio.h>
+#include <ctype.h>
+/**
+ * main - a program that we're going to print combinaison of n
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(', ');
+	} putchar($);
+	putchar('\n');
+	return (0);
+}

6ee803fdd1f3439b121bda34777b47c4cf887b1c
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 06:07:02 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,18 @@ int main(void)
 
 	for (j = 0; j <= 99; j++)
 	{
-		putchar(j / 10 + '0');
-		putchar(j % 10 + '0');
-	}
-	for (k = 0; k <= 99; k++)
-	{
-		putchar(k / 10 + '0');
-		putchar(k % 10 + '0');
-	}
-	if (j != 98 || k != 99)
-	{
-		putchar(',');
-		putchar(' ');
+		for (k = 0; k <= 99; k++)
+			{ 
+				 putchar(j / 10 + '0');
+				 putchar(j % 10 + '0');
+				 putchar(k / 10 + '0');
+				 putchar(k % 10 + '0');
+				 if (j != 98 || k != 99)
+				 {
+					 putchar(',');
+					 putchar(' ');
+				 }
+			}
 	} putchar('\n');
 	return (0);
 }

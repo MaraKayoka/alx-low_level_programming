d2f4ebc995b64ce21537f6e3236382aabaf0a369
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 02:23:14 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - a program that prints the alphabet except two letters
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	char ch = 'A';
+
+	ch = tolower(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++;)
+	{
+		if (ch != 'Q' && ch != 'E')
+		putchar(tolower(ch));
+	} putchar('\n')
+	return (0);
+}

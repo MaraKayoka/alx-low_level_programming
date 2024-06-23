f55d2f740fa37f1b6141df255e1e1556ceb4d243
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 00:22:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - a program that prints the alphabet in lower case
+ * Return: Always 0(Success)
+ */
+/* Bettyu checking requires an extra line for the expression*/
+
+int main(void)
+{
+	char ch ='A';
+	for (ch = 'A', ch <= 'Z'; ch++);
+	
+	putchar(ch);
+	return (0);
+}	

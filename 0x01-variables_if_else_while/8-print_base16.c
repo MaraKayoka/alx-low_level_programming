56d23a1dc9e431b3d9f44e876f26f3ffc4fad1cd
Mara Kayoka <marakayoka@gmail.com>
2024-06-23 03:20:35 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - a program that print hexadecimal numbers
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int n;
+	char ch = 'A';
+
+	ch = tolower(ch);
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'A'; ch <= 'F'; ch++)
+	{
+		putchar(tolower(ch));
+	}
+
+	return (0);
+}

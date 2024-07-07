f8b4d5e3abb140e4d9ce3ceaf05eff84a6e6a5b1
Mara Kayoka <marakayoka@gmail.com>
2024-07-07 05:36:38 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - a program that computes the absolute value of a number
+ * @p: The input integer.
+ * Return: 0(Always)
+ */
+int _abs(int p)
+{
+	if (p >= 0)
+	{
+		return (p);
+	} return (-p);
+
+}

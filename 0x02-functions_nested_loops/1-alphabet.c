efa0101b7e5d6b040857c8719466c3132d1422ab
Mara Kayoka <marakayoka@gmail.com>
2024-07-07 02:56:34 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-gghgfh
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints the alphabet in lower case
+ * Return: 0(Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}

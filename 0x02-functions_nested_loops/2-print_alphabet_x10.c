ef59273bf3d387464052c4a1a0ce1f771577fdc3
Mara Kayoka <marakayoka@gmail.com>
2024-07-07 03:25:05 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 time
+ *Return: 0(Success)
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	} putchar('\n');
+}

4d600c557f7e7e10dc6170df43999fd08d296d58
Mara Kayoka <marakayoka@gmail.com>
2024-07-10 16:58:35 -0700

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  _pow_recursion - a function that returns the value of an int
+ * @x: integer where the value should be taken
+ * @y: integer where the value will be raised
+ *  Return: factorial
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	} else if (y == 0)
+	{
+		return (1);
+	} else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
